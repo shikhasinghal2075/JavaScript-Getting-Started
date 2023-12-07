6ece30dbc973080d777d28375ca9c74dbe55db6c
Shikha Singhal <60216456+shikhasinghal2075@users.noreply.github.com>
2023-12-07 09:41:19 +0000

    Comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,10 @@
+
+// unpredictab;e outputs --> Not recommended
+console.log("2" > 1)    // true
+console.log("02" > 1)   // true
+
+console.log(null > 0)    // false
+console.log(null == 0)    // false
+console.log(null >= 0)    // true
+
+// We have strict check (===) --> Not only checks values but also data type
