262db3bd0e80b72427e24faa1798486cf3b6f247
Shikha Singhal <60216456+shikhasinghal2075@users.noreply.github.com>
2023-12-07 05:42:59 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,25 @@
 "use strict"; //treat all js code as newer version
 
 // alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Shikha")
+
+let name = "Shikha"
+let age = 25
+let isLoggedIn = true
+
+/* Datatypes
+- number
+- bigInt
+- string
+- boolean
+- null => standalone value
+- symbol
+- object
+*/
+
+console.log(typeof age)
+// console.log(typeof null) // type object
+// console.log(typeof undefined) // undefined
+
