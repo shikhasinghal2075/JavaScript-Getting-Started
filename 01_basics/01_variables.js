c8929ed6cf5a287389fbb3431fa0bc725c4e506b
Shikha Singhal <60216456+shikhasinghal2075@users.noreply.github.com>
2023-12-07 05:34:22 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "test@gmail.com"
+//  var accountPassword = "12345"
+let accountPassword = "12345"
+let accountCity = "Chandigarh"
+/*
+Prefer not to use var  
+because of issue in block and 
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
