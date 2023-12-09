7a182cf5da13a68a605cb37b1e0d0b300b103f15
Shikha Singhal <60216456+shikhasinghal2075@users.noreply.github.com>
2023-12-09 11:36:22 +0000

    loops part2

--- 03_iterations/02_loops.js
+++ 03_iterations/02_loops.js
@@ -21,3 +21,32 @@ const myCoding = [
 myCoding.forEach((item) => {
     console.log(item.language)
 })
+
+// const coding = ["js","cpp","c"]
+// const values = coding.forEach((item) => {
+//     return item;
+// })
+
+// console.log(values)
+
+const myNums = [1,2,4,5,0,8,9,4]
+const newNums = myNums.filter((num) => num>4)
+console.log(newNums)        //[ 5, 8, 9 ]
+
+// OR
+
+const newNums2 = []
+myNums.forEach((num) => {
+    if(num > 4){
+        newNums2.push(num)
+    }
+})
+console.log(newNums2)        //[ 5, 8, 9 ]
+
+const newNums3 = myNums.map((num) => num+10)
+console.log(newNums3)
+
+// chaining --> use 2 or more methods together --> result of first gets passed on to second method
+const newNums4 = myNums.map((num) => num+10).map((num) => num+1).filter((num) => num >= 4)
+console.log(newNums4)
+
